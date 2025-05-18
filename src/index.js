c87f0d0de3afffacae44d49acdcdac4294348251
ApramPepo <120373555+ApramPepo@users.noreply.github.com>
2025-05-19 01:24:58 +0300

    npm & webpackging

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import './styles.css';
+
+const greeting = "Hello, Odinite";
+document.getElementById('app').innerHTML = `<h1>${greeting}</h1>`;
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const plugin = require('tailwindcss');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        clean: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/i,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                },
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+        }),
+    ],
+    devServer: {
+        static: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 8080,
+    },
+    mode: 'development',
+};
